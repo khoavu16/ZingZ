07bc9e178036100683f5daf78a4da70a7a27e089
vu le minh khoa <you@example.com>
2023-06-18 18:10:23 +0700

    update màu

--- lib/network/styles/themes/app_colors.dart
+++ lib/network/styles/themes/app_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const zingBackground = Color(0xff660099);
+  static const zingBackground = Color(0xff72179a);
   static const white = Colors.white;
 }
